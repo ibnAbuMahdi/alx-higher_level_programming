bf43c9e973a6cef23061e2d369bcc5f96609b3a0
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-02 01:34:42 -0800

    5.1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,8 +3,8 @@ from calculate_1 import add
 from calculate_1 import sub
 from calculate_1 import div
 from calculate_1 import mul
-a = 1
-b = 2
+a = 10
+b = 5
 if __name__ == "__main__":
     print("{} + {} = {}".format(int(a), int(b), int(add(a, b))))
     print("{} - {} = {}".format(int(a), int(b), int(sub(a, b))))
