a61544ff21e15f098792032cd8ba0e5bf58c726e
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-19 18:43:01 -0800

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = j = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+            i += 1
+        except (TypeError, ValueError):
+           i += 1
+           continue
+    print("")
+    return j
+
