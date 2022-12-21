1de8c5b24e27272c56396fda831caa71e61112db
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-20 20:17:45 -0800

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" module for the square class """
+
+
+class Square:
+    """ The square class and all its fields """
+    def __init__(self, size=0):
+
+        """ the init method """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """int: the area of the square """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """ the object size getter """
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = val
