2fd96578e33743f296115025e34515a1ccf77ee2
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-26 12:23:56 -0800

    still on None node

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -70,9 +70,8 @@ class SinglyLinkedList:
 
     def __str__(self):
         """ the string representation of the list """
-        if self.__head is not None:
-            temp = self.__head
-            while temp.next_node is not None:
-                print(temp.data)
-                temp = temp.next_node
-            return str(temp.data)
+        temp = self.__head
+        while temp is not None and temp.next_node is not None:
+            print(temp.data)
+            temp = temp.next_node
+        return str(temp.data)
