f6d39d0b1addac5543bf581bc65ad0db2c8d32f1
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-26 11:14:37 -0800

    corrected 13 for large list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -32,7 +32,7 @@ int is_palindrome(listint_t **h)
 
 	if (len == 1 || len == 0)
 		return (1);
-	return (compare(*h, 0, len - 1));
+	return (compare(*h, 0, len - 1, 0));
 }
 
 /**
@@ -40,24 +40,26 @@ int is_palindrome(listint_t **h)
  * @h: the head node
  * @i: index of first node
  * @f: index of second node
+ * @d: no. of nodes traversed
  * Return: 1 if they are equal else 0
  */
 
-int compare(listint_t *h, int i, int f)
+int compare(listint_t *h, int i, int f, int d)
 {
 	listint_t *node_i, *node_f;
 
 	if (i == f)
 		return (1);
 
-	node_i = get_nodeint_at_index(h, i);
-	node_f = get_nodeint_at_index(h, f);
+	node_i = get_nodeint_at_index(h, i - d);
+	node_f = get_nodeint_at_index(h, f - d);
 
 	if (i == f - 1)
 		return (node_i->n == node_f->n);
 	if (node_i->n != node_f->n)
 		return (0);
-	return (compare(h, i + 1, f - 1));
+	d++;
+	return (compare(h->next, i + 1, f - 1, d));
 }
 
 /**
@@ -85,9 +87,3 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 	return (NULL);
 }
-
-
-
-
-
-
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -21,7 +21,7 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *, unsigned int);
-int compare(listint_t *, int, int);
+int compare(listint_t *, int, int, int);
 size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */
