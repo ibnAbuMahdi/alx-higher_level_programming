f86d39ade7f6b27879dcb49990b89c6cefc03ea6
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-27 08:18:30 -0800

    made radius optional in magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@
 class MagicClass:
     """ where the magic happens """
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """ the init method """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
