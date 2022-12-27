4351cf7799efcce4946aa570917f5ee0903a8ede
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-27 08:16:25 -0800

    did magic class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A module for a class that does magic """
+
+
+class MagicClass:
+    """ where the magic happens """
+
+    def __init__(self, radius):
+        """ the init method """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """ returns the area """
+        return self.__radius**2 * math.pi
+
+    def circumference(self):
+        """ returns the circumference """
+        return 2 * math.pi * self.__radius
