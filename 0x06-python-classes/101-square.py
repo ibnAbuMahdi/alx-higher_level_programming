81fea40df347318267a608529a09ca570ad1365a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2022-12-26 20:38:55 -0800

    for task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+""" module for the square class """
+
+
+class Square:
+    """ The square class and all its fields """
+    def __init__(self, size=0, pos=(0, 0)):
+
+        """ the init method """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        if self.isTuple(pos):
+            self.__pos = pos
+        else:
+            raise TypeError("position must be a tuple of 2 \
+positive integers")
+
+    def area(self):
+        """int: the area of the square """
+        return self.__size**2
+
+    @property
+    def size(self):
+        """ the object size getter """
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = val
+
+    @property
+    def position(self):
+        """ the tuple of the position """
+        return self.__pos
+
+    @position.setter
+    def position(self, val):
+        if self.isTuple(val):
+            self.__pos = val
+        else:
+            raise TypeError("position must be a tuple \
+of 2 positive integers")
+
+    def my_print(self):
+        """ prints the square using # """
+        if self.__size > 0:
+            if self.__pos[1] > 0:
+                print("\n"*(self.__pos[1]), end="")
+            for i in range(self.__size):
+                print(" "*self.__pos[0] + "#"*self.__size)
+        else:
+            print("")
+
+    def isTuple(self, val):
+        """ check whether val is the right tuple """
+
+        if type(val) is not tuple:
+            return False
+        if len(val) != 2:
+            return False
+        if type(val[0]) is not int or type(val[1]) is not int:
+            return False
+        if val[0] < 0 or val[1] < 0:
+            return False
+        return True
+
+    def __str__(self):
+        """ behaves like my_print() """
+
+        if self.__size > 0:
+            if self.__pos[1] > 0:
+                print("\n"*self.__pos[1], end="")
+            for i in range(self.__size - 1):
+                print(" "*self.__pos[0] + "#"*self.__size)
+            return " "*self.__pos[0] + "#"*self.__size
+        else:
+            return ""
--- 0x06-python-classes/m6.py
+++ 0x06-python-classes/m6.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-Square = __import__('6-square').Square
+Square = __import__('101-square').Square
 
 
 
@@ -14,7 +14,7 @@ print("--")
 
 
 
-my_square_2 = Square(3, (1,1))
+my_square_2 = Square(1, (1,1))
 
 my_square_2.my_print()
 
@@ -26,7 +26,7 @@ print("--")
 
 my_square_3 = Square(3, (3,2))
 
-my_square_3.my_print()
+print(my_square_3)
 
 
 
