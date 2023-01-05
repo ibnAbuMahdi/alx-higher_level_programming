1aec52dcc27f71262ea915fa38e7faa9cc3896f0
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-05 00:17:28 -0800

    N QUEENS! - my first serious python coding challenge

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -17,15 +17,57 @@ if N < 4:
     print("N must be at least 4")
     exit(1)
 
+# initialize solution list, start column, occupied columns list,
+# occupied left diagonal list and occupied right diagonal list
 sol = []
-st = 0
+i = j = st = 0
 col = []
 ldiag = []
 rdiag = []
-for i in range(N):
-    for j in range(st, N):
-        tm = min(j + i, N)
-        tm2 = max(0, i - (N - j))
-        if j not in col and i - j not in ldiag and i - (N - j):
+
+# loop for  rows
+while i < N:
+
+    # loop for columns
+    while j < N:
+
+        # cell coordinates for rdiag
+        cc = min(j + i, N)
+        rc = max(0, i - (N - j))
+
+        # check for similar column, rdiag or ldiag
+        if j not in col and i - j not in ldiag and [rc, cc] not in rdiag:
+            ldiag.append(i - j)
+            rdiag.append([rc, cc])
+            col.append(j)
             sol.append([i, j])
+            j = 0
             break
+        j += 1
+
+    # if a solution is found, backtrack to the first row and next column
+    if len(sol) == N:
+        j = sol[0][1] + 1
+        i = 0
+        print(sol)
+        sol.clear()
+        ldiag.clear()
+        rdiag.clear()
+        col.clear()
+
+    # if next valid position is not found, backtrack to the previous
+    # queen and move it to the next column and same row and delete
+    # all its diags and column
+    elif i > 0 and len(sol) > 0 and len(sol) == ln:
+        top = sol.pop()
+        j = top[1] + 1
+        i -= 1
+        col.remove(top[1])
+        ldiag.pop()
+        rdiag.pop()
+        ln -= 1
+
+    # else, i.e. a valid position is found, update ln
+    else:
+        i += 1
+        ln = len(sol)
---
+++ 0x08-python-more_classes/tst.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = n = 0
+while i < 5:
+    if n == 4:
+        print("i: {} - n: {}".format(i, n))
+        break
+    n += 1
+    i = 0
