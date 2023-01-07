2b5e5c0ae558b234d97c92703ef98029ffbddacd
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-06 20:52:01 -0800

    doc-ed 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,6 +3,8 @@
 
 
 def print_square(size):
+    """ prints a square with # """
+
     if type(size) is float and size < 0:
         raise TypeError('size must be an integer')
     if type(size) is not int:
