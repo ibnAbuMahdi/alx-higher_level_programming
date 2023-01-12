33ee9079a038fd401e43cc1544c31c4f0e0085a2
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-11 17:41:49 -0800

    pycodestyled 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,8 +4,8 @@
 
 def append_write(filename="", text=""):
     """ appends text to a file with name filename """
-    
+
     n = 0
     with open(filename, "a", encoding="utf-8") as f:
-            n = f.write(text)
-    return n 
+        n = f.write(text)
+    return n
