cabfb44b713ca07fb71b895742ae9d443f9f2cd8
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-01-13 07:14:09 -0800

    lets see

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -28,8 +28,8 @@ def isvalid(line):
     if not isinstance(line, str):
         return 0
     parts = line.split()
-    if not parts[4].isdigit() or not parts[5].isdigit():
-        return 0
+    #if not parts[4].isdigit() or not parts[5].isdigit():
+     #   return 0
     ip = parts[0].split(".")
     for n in ip:
         if not n.isdigit():
