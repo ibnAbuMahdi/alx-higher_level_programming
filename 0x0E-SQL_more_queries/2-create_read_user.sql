c1b71cbf4ce9bcaf4e57d1168f66a7eac0a7072b
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-02-16 04:32:51 -0800

    crxn 1 for 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,10 +3,13 @@
 -- create db hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+-- drop user if exists
+DROP USER IF EXISTS 'user_0d_2'@'localhost';
+
 -- create user user_0d_2
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant select privilege to user_0d_2
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
 
 
