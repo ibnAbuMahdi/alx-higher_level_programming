e725b1db2912f102fe25435f8a6e2964658b11b2
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-02-16 09:06:35 -0800

    crxn 3 for 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 --14 average
 
 --computes the everage of scores from second_table
-SELECT AVG(score) AS `average` FROM second_table
+SELECT AVG(second_table.score) AS second_table.average FROM second_table
