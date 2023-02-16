e008a7b4d9460ba293ed02f334b0accd348522de
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-02-15 20:47:05 -0800

    for 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,9 +5,9 @@ CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	NAME varchar(256),
 	score INT
-)
+);
 
 -- insert values into second_table
 INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10), VALUES (2, "Alex", 3),
-VALUES (3, "Bob", 14), VALUES (4, "George", 8)
+VALUES (1, "John", 10), (2, "Alex", 3),
+(3, "Bob", 14), (4, "George", 8)
