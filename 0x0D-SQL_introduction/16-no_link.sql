f78ecdc304dc9adcef87c5ffa48c65a597fe03df
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-02-16 06:24:40 -0800

    crxn 1 for 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --16 no link
 
 --list all record of second table 
-SELECT score, name FROM second_table WHERE name != NULL ORDER BY score DESC
+SELECT score, name FROM second_table WHERE name<>'' ORDER BY score DESC
