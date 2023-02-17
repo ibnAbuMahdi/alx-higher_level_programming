96c703c14f4fec4a3babcf3ad2c8ef50f021b8e2
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-02-16 21:01:05 -0800

    f

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,4 @@
+-- 10
+
+-- join show and show genre
+SELECT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id INNER JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_genres.name='Comedy' ORDER BY tv_shows.title ASC
