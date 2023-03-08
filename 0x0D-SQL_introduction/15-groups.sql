ecc95eb25fbd0986190c59172fee493d7b3e54a7
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-07 16:53:12 -0800

    lets see

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 --15 groups
 
 --list scores grouped and sorted by frequency in second_table
-SELECT score, COUNT(score) AS number GROUP BY score, number ORDER BY number DESC
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY COUNT(score) DESC
