4864cc095f03aa0301f187c6072869ce5e30a3cd
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-13 22:06:33 -0700

    for 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const num = Number(args[2]);
+if (!isNaN(parseFloat(num)) && isFinite(num)) {
+  for (let i = 0; i < parseInt(num); i++) {
+    for (let j = 0; j< parseInt(num); j++) {
+      process.stdout.write('X');
+    }
+    process.stdout.write('\n');
+  }
+} else {
+  console.log('Missing size');
+}
