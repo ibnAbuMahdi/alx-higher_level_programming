3e2b80f53e55fc0606f76259453d87b1b3a530d4
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 05:30:58 -0700

    for 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+import urllib.parse
+import urllib.error
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as resp:
+            page = resp.read()
+            print(page.decode('utf-8'))
+    except urllib.error.HTTPError as e :
+        print("Error code: {}".format(e.code))
