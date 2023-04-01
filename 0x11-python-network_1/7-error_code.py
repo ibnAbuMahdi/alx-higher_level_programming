03ffd67149ac6559cbc3003dfb83a6f96f547bde
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 09:47:33 -0700

    crxn 1 for 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,9 +3,10 @@
 import requests
 from sys import argv as args
 
-data = requests.get(args[1])
+if __name__ == "__main__":
+    data = requests.get(args[1])
 
-if data.status_code >= 400:
-    print("Error code: {}".format(data.status_code))
-else:
-    print(data.text)
+    if data.status_code >= 400:
+        print("Error code: {}".format(data.status_code))
+    else:
+        print(data.text)
