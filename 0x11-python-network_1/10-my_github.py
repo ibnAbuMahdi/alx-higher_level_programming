8311432a768e2a8c20c847489e0dd769fb2fec57
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 10:31:22 -0700

    crxn 2 for 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,8 @@ from sys import argv as args
 
 if __name__ == "__main__":
     headers = {"Accept": "application/vnd.github+json",
-               "Authorization": "Bearer {}".format(args[2])}
+               "Authorization": "Bearer {}".format(args[2]),
+               "X-GitHub-Api-Version": "2022-11-28"}
     data = requests.get("https://api.github.com/{}".format(args[1]),
                         headers=headers)
     print(data.json())
