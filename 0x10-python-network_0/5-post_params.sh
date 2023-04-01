665cd56fd2e641b22fcdc57b407fc9cc80d0d11f
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-03-31 19:43:28 -0700

    crxn for 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #print body only if response code is 200
-curl -s -d "email=test@gmail.com&subject='I will always be here for PLD'" "$1" 
+curl -s -d "email=test@gmail.com&subject='I%20will%20always%20be%20here%20for%20PLD'" "$1" 
