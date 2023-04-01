b0a6d2fde6da5b53ee3d1eee5cec3b64f23fe6af
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 10:01:24 -0700

    crxn 2 for 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,4 @@ from sys import argv as args
 
 if __name__ == "__main__":
     data = requests.get(args[1])
-    if 'X-Request-Id' in data.headers['X-Request-Id']:
-        print(data.headers['X-Request-Id'])
+    print(data.headers['X-Request-Id'])
