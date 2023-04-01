029c6a0c70c0e7319c2256d93c3f837197d8d744
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 09:52:31 -0700

    crxn 1 for 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,14 +3,15 @@
 import requests
 from sys import argv as args
 
-val = args[1] if len(args) > 1 else ""
-data = requests.post("http://0.0.0.0:5000/search_user", {'q': val})
+if __name__ == "__main__":
+    val = args[1] if len(args) > 1 else ""
+    data = requests.post("http://0.0.0.0:5000/search_user", {'q': val})
 
-try:
-    j = data.json()
-    if len(j) == 0:
-        print("No result")
-    elif 'id' in j and 'name' in j:
-        print("[{}] {}".format(j['id'], j['name']))
-except requests.exceptions.JSONDecodeError as e:
-    print("Not a valid JSON")
+    try:
+        j = data.json()
+        if len(j) == 0:
+            print("No result")
+        elif 'id' in j and 'name' in j:
+            print("[{}] {}".format(j['id'], j['name']))
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
