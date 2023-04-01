839b0d5f67788eae2ddd6448a412ef5cdb221c08
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 05:15:12 -0700

    crxn 1 for 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         data = resp.read()
-        info = resp.info()
     print("Body response:")
-    print("\t- type:".expandtabs(4), type(data))
-    print("\t- content:".expandtabs(4), data)
-    if "utf-8" in info['Content-Type']:
-        print("\t- utf8 content: OK".expandtabs(4))
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content: {}".format(data.decode('utf-8')))
