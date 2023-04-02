51fc90d96b2425dff3240e0f91b0ce93e11ce4d7
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 21:36:07 -0700

    docd model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 """ model_state class """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import relationship
-from sqlalchemy.engine.url import URL
-Base = declarative_base()
-db_url = {'drivername': 'mysql',
-          'username': 'root',
-          'password': 'root',
-          'host': 'localhost',
-          'port': 3306}
-engine = create_engine('mysql+mysqldb://root:root@localhost:\
-                       3306/hbtn_0e_6_usa', pool_pre_ping=True)
-
 if __name__ == "__main__":
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import relationship
+    from sqlalchemy.engine.url import URL
+    Base = declarative_base()
+    engine = create_engine('mysql+mysqldb://root:root@localhost:\
+                           3306/hbtn_0e_6_usa', pool_pre_ping=True)
+
     class State(Base):
         """ State class that inherits Base """
 
