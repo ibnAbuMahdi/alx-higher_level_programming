39a638f71524e968d24596b8cea6fdba64016f08
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 22:22:35 -0700

    docd model_state 2

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ model_state class """
-if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import relationship
-    from sqlalchemy.engine.url import URL
-    Base = declarative_base()
-    engine = create_engine('mysql+mysqldb://root:root@localhost:\
-                           3306/hbtn_0e_6_usa', pool_pre_ping=True)
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import relationship
+from sqlalchemy.engine.url import URL
+Base = declarative_base()
+engine = create_engine('mysql+mysqldb://root:root@localhost:\
+                       3306/hbtn_0e_6_usa', pool_pre_ping=True)
 
-    class State(Base):
-        """ State class that inherits Base """
+class State(Base):
+    """ State class that inherits Base """
 
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
 
-    Base.metadata.create_all(bind=engine)
