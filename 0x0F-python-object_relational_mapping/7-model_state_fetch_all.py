0ce6b83eedb30458394b1ebc9e313328cc8d495b
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-01 21:31:26 -0700

    crxn 3 for 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Start link class to table in database
 """
-import sys
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
-
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+
     if len(sys.argv) == 4:
         engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                                .format(sys.argv[1], sys.argv[2],
