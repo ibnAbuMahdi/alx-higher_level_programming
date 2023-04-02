9fd97fd93d86aa7172e3c618f6d8067adaf5c8ca
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-02 00:06:14 -0700

    crxn 3 for 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,9 +16,14 @@ if len(sys.argv) == 4:
         Session = sessionmaker()
         Session.configure(bind=engine)
         session = Session()
-
-        query = session.query(State).order_by(State.id)
-        for state in query.all():
-            print("{}: {}".format(state.id, state.name))
-            for city in state.cities:
-                print("    {}: {}".format(city.id, city.name))
+for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
+#
+#       query = session.query(State).order_by(State.id)
+#        for state in query.all():
+#            print("{}: {}".format(state.id, state.name))
+#            for city in state.cities:
+#                print("    {}: {}".format(city.id, city.name))
