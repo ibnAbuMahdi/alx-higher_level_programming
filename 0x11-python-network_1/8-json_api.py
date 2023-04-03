8fa5a5d78dfc4252a337cad6405fadf3e4463884
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-02 17:00:31 -0700

    crxn 6 for 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,15 +7,13 @@ if __name__ == "__main__":
     val = args[1] if len(args) > 1 else ""
     data = requests.post("http://0.0.0.0:5000/search_user", {'q': val})
 
-    try:
+    if data.headers.get('content-type') != 'application/json':
+        print("Not a valid JSON")
+    else:
         j = data.json()
-        if data.headers.get('content-type') != 'application/json':
-            print("Not a valid JSON")
-        elif len(j) == 0:
+        if len(j) == 0:
             print("No result")
         elif 'id' in j and 'name' in j:
             print("[{}] {}".format(j['id'], j['name']))
         else:
             print("Not a valid JSON")
-    except requests.exceptions.JSONDecodeError as e:
-        print("Not a valid JSON")
