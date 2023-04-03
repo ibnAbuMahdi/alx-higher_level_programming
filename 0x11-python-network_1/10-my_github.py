4bd95762f2add0fd6909f25ccadbf273bae0bb13
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-02 18:05:47 -0700

    crxn 3 for 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,11 +2,12 @@
 """ Fetches https://alx-intranet.hbtn.io/status """
 import requests
 from sys import argv as args
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     headers = {"Accept": "application/vnd.github+json",
                "Authorization": "Bearer {}".format(args[2]),
                "X-GitHub-Api-Version": "2022-11-28"}
-    data = requests.get("https://api.github.com/{}".format(args[1]),
+    data = requests.get("https://api.github.com/users/{}".format(args[1]),
                         headers=headers)
-    print(data.json())
+    print(data.json()['id'])
