2c88e2783dabb71a72763a1fdb7683fd10749acd
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-05 09:02:55 -0700

    crxn 4 for 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits"
     data = requests.get(url.format(args[1], args[2]))
     cmts = data.json()
-    for cmt in list(data.json())[:10]:
-        print("{}: {}".format(dict(cmt).get('sha'),
-                              dict(cmt).get('commit').get('author').get('name')))
+    for cmt in data.json()[:10]:
+        print("{}: {}".format(cmt.get('sha'),
+                              cmt.get('commit').get('author').get('name')))
