13ac20e6d54d4a680b43dfc2f7c838e1743e0b79
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-04-04 17:00:56 -0700

    crxn 9 for 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,22 +4,9 @@ import requests
 from sys import argv as args
 
 if __name__ == "__main__":
-    headers = {"Accept": "application/vnd.github+json"}
     url = "https://api.github.com/repos/{}/{}/commits"
     data = requests.get(url.format(args[1], args[2]))
     cmts = data.json()
-    cmts_dict = {}
-    i = 1
-    for cmt in cmts:
-        print("{}: {}".format(cmt.get('sha'), cmt.get('commit').get('author').get('name')))
-#        cmts_dict[cmt['commit']['author']['date']] =\
-#                [cmt['sha'], cmt['commit']['author']['name']]
-        if i == 10:
-            break
-        else:
-            i += 1
-    cmts_keys = list(cmts_dict.keys())
-    cmts_keys.sort(reverse=True)
-    cmts_sorted = {i: cmts_dict[i] for i in cmts_keys}
-#    for k, v in cmts_sorted.items():
-#        print("{}: {}".format(v[0], v[1]))
+    for cmt in cmts[:10]:
+        print("{}: {}".format(cmt.get('sha'),
+                              cmt.get('commit').get('author').get('name')))
